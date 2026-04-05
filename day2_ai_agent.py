e36e014df6e3d7fef7f54f60a93c30507fe54ff1
Anshika <anshikaverma42628@gmail.com>
2026-04-05 16:33:44 +0530

    feat: add day 2 ai agent script

---
+++ day2_ai_agent.py
@@ -0,0 +1,15 @@
+# Lists
+Companies = ["Google", "Meta", "OpenAi"]
+Companies.append("Anthropic")
+
+# Dictionaries
+my_profile = {
+    "name": "Anshika",
+    "day": "1",
+    "skill_level": "1"
+}
+
+my_profile["skill_level"] = "2"
+
+print(f"I am {my_profile['name']}.My level is {my_profile['skill_level']}")
+print(f"I am studying at {Companies[0]}")
